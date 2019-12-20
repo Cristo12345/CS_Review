02b061cbd99a52ddb8aa254cac293558e231b8ba
Christian Suleiman <cisuleiman@gmail.com>
2019-12-19 21:38:01 -0500

    insertion sort file added

---
+++ Sorting_Algos/insertionSort.cpp
@@ -0,0 +1,14 @@
+// Similar to sorting cards on a table
+// Split array into two sections, SORTED and UNSORTED
+// At start, SORTED is just the first element. With each iteration, we will move the marker for SORTED to the next element
+// and insert that element in the correct position.
+// After going passing through array once, each elemnt should be in correct position
+
+// Pre: arr is an array of unique integers
+// Post: return arr sorted in ascending order
+
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], )
