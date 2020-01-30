3bf447d4ecdf2bf7a851e0287cb24336357ff07f
Christian Suleiman <cisuleiman@gmail.com>
2020-01-30 16:03:55 -0500

    bubbleSort init

---
+++ Sorting_Algos/bubbleSort.cpp
@@ -0,0 +1,14 @@
+// Bubblesort goes through the entire array, checks the current element and the next one, then swaps them if they
+// are not in correct order. By this method, we will eventually put all elements in correct place. 
+// We mus perform an entire pass without needing to swap before we can be sure the array is sorted. As such, this method
+// is inefficient as we must make another pass after the array is sorted. 
+
+// Pre: arr is an array of unique integers, n is the size of the array
+// Post: return arr sorted in ascending order
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+
+}
