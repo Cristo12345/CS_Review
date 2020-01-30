9f369c75cdd0ac71a9381ece8b8955c00396cb9c
Christian Suleiman <cisuleiman@gmail.com>
2020-01-30 16:56:26 -0500

    fixed bubbleSort's inner loop upper bound

--- Sorting_Algos/bubbleSort.cpp
+++ Sorting_Algos/bubbleSort.cpp
@@ -17,7 +17,7 @@ void bubbleSort(int arr[], int size) {
     for (int i = 0; i < size && !isSorted; i++) {
         isSorted = true; 
 
-        for (int j = 0; j < size; j++) {
+        for (int j = 0; j < size-1; j++) {
             if (arr[j] > arr[j+1]) {
                 int tmp = arr[j];
                 arr[j] = arr[j+1];
