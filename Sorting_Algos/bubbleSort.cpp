1bec5f1e9dcd91a104158d8cdc2ac740f5efa967
Christian Suleiman <cisuleiman@gmail.com>
2020-01-30 16:45:58 -0500

    first bubbleSort + main()

--- Sorting_Algos/bubbleSort.cpp
+++ Sorting_Algos/bubbleSort.cpp
@@ -1,6 +1,7 @@
 // Bubblesort goes through the entire array, checks the current element and the next one, then swaps them if they
 // are not in correct order. By this method, we will eventually put all elements in correct place. 
-// We mus perform an entire pass without needing to swap before we can be sure the array is sorted. As such, this method
+// At the end of the kth pass, the kth largest element will be in the correct place
+// We must perform an entire pass without needing to swap before we can be sure the array is sorted. As such, this method
 // is inefficient as we must make another pass after the array is sorted. 
 
 // Pre: arr is an array of unique integers, n is the size of the array
@@ -11,4 +12,39 @@ using namespace std;
 
 void bubbleSort(int arr[], int size) {
 
+    bool isSorted;
+
+    for (int i = 0; i < size && !isSorted; i++) {
+        isSorted = true; 
+
+        for (int j = 0; j < size; j++) {
+            if (arr[j] > arr[j+1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                isSorted = false;
+            }
+        }
+    }
+
+    return;
+}
+
+int main() {
+    int myArr[] = {6, 2, 7, 11, 5, 8};
+    int size = (sizeof(myArr)/sizeof(*myArr));
+
+    cout << "Original array is \n";
+    for (int i = 0; i < size; i++) {
+        cout << myArr[i] << ' ';
+    }
+
+    bubbleSort(myArr, size);
+
+    cout << "\nSorted array is \n";
+    for (int j = 0; j < size; j++) {
+        cout << myArr[j] << ' ';
+    }
+
+    return 0;
 }
