170c1658a0c714b23ca71416ca8e23148076d850
Christian Suleiman <cisuleiman@gmail.com>
2020-03-18 19:18:21 -0400

    added method 2 to isPerm problem

--- Arrays&Strings/permutation.cpp
+++ Arrays&Strings/permutation.cpp
@@ -30,15 +30,57 @@ bool isPerm(string str1, string str2)
     return true;
 }
 
+// Method 2
+// count the characters in each string then compare
+
+// Runtime: O(n)
+
+bool isPerm2(string str1, string str2)
+{
+
+    // permutations must be same length so we can do a quick check first
+    if (str1.length() != str2.length())
+    {
+        return false;
+    }
+
+    // assuming extended-ASCII, we have 256 possible chars
+    // create an array holding number of times each char is found for each string
+    int str1Chars[256] = {0};
+    int str2Chars[256] = {0};
+
+    // go through both strings and count their chars
+    for (int i = 0; str1[i] && str2[i]; i++)
+    {
+        str1Chars[str1[i]]++;
+        str2Chars[str2[i]]++;
+    }
+
+    // compare the two arrays
+    for (int i = 0; i <= 255; i++)
+    {
+        if (str1Chars[i] != str2Chars[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
 int main()
 {
     string str1 = "god";
     string str2 = "dog";
 
-    if (isPerm(str1, str2)) {
+    if (isPerm2(str1, str2))
+    {
         cout << "permutation found";
     }
-    else {
+    else
+    {
         cout << "no permutation";
     }
 
