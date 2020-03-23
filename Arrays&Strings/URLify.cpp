d7cfaffdc87fa534af86ea71c0eba05ac3bedc9e
Christian Suleiman <cisuleiman@gmail.com>
2020-03-23 16:23:11 -0400

    added URLify problem

---
+++ Arrays&Strings/URLify.cpp
@@ -0,0 +1,14 @@
+// Write a method that replaces all spaces in a string with '%20'. Assume the
+// string has neough space to hold the additional chars and you are
+// given the "true length" of the string
+
+// IN: "Mr John Smith    "
+// OUT: "Mr%20John%20Smith"
+
+// NOTE: A common approach for manipulation strings is to start editing
+// from the end and working backwards. Helpful if we have extra buffer
+// space in the end.
+
+using namespace std;
+#include <string>
+
