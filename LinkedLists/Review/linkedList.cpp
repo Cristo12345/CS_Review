7764ba2358912182b7ffc0c39348bde6ef854abe
Christian Suleiman <cisuleiman@gmail.com>
2020-03-26 20:26:35 -0400

    added Linked List folders

---
+++ LinkedLists/Review/linkedList.cpp
@@ -0,0 +1,8 @@
+// Linked Lists
+// A linear data structure. Made up of nodes that aren't necessarily stored next to each other in memory
+// Each node contains a value and a pointer to next node. Points to NULL if last node. 
+
+// [ A ] -> [ B ] -> [ C ] -> NULL
+
+
+    
