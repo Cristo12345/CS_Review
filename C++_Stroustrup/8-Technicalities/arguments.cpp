c0d106f4167d2bf116f5ace64b7c54cae1a8e018
Christian Suleiman <cisuleiman@gmail.com>
2020-07-14 19:59:39 -0400

    argument pass examples chpt 8

--- C++_Stroustrup/8-Technicalities/arguments.cpp
+++ C++_Stroustrup/8-Technicalities/arguments.cpp
@@ -6,18 +6,50 @@ using namespace std;
 
     // PASS BY VALUE: make a copy of the argument and pass the copy into the function, does not alter original value (val that was copied)
     // good for quick computations. NOT GOOD when making a copy of something large (like an image)
-    int fval(int x)
+    int f_val(int x)
     {
+        // new local copy of x
+        x = x + 1;
+        return x;
+    }
+
+    
+    // PASS BY REFERENCE: point to the address of the variable we are using
+    // this will modify the actual value of the argument we pass in
+    int f_ref(int& x) {
         x = x + 1;
         return x;
     }
 
 
+    // PASS BY CONST REFERENCE: point to the address of the variable we are using
+    // this will NOT modify the actual value of the argument we pass in (thanks to the CONST)
+    // trying to modify x's value in the function will cause error
+    int f_const_ref(const int& x) {
+        // x = x + 1; this line will cause an error
+        int xx = x + 1;
+        return xx;
+    }
+
 
 int main()
 {
     int x = 0;
+
+    // PASS BY VAL TEST
+    // cout << x << endl;
+    // cout << f_val(x) << endl;   
+    // cout << x << endl; //  value of global x is unchanged
+
+
+    // PASS BY REF TEST
     cout << x << endl;
-    cout << fval(x) << endl;   
-    cout << x << endl; // original value of x is unchanged
+    cout << f_ref(x) << endl;
+    cout << x << endl; // value of global x is changed
+
+    // PASS BY CONST REF TEST
+    // cout << x << endl;
+    // cout << f_const_ref(x) << endl;
+    // cout << x << endl; // value of global x is  unchanged
+
 }
